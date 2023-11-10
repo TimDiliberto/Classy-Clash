5b33dac57bf19b01f586d89de8fa9531b2854062
Tim Diliberto <timothydiliberto19@gmail.com>
2023-11-09 20:33:00 -0800

    Added character

--- main.cpp
+++ main.cpp
@@ -7,15 +7,21 @@
 int main()
 {
     // Initialize window dimensions
-    const int winDims[] {384, 384};
+    const float winDims[] {384, 384};
     InitWindow(winDims[0], winDims[1], "Classy Clash");
 
     // Set background image and position
     Texture2D map =
         LoadTexture("nature_tileset/(32x32) ClassyClash_worldMap.png");
     Vector2 mapPos{0.0, 0.0};
-
+    // Movement speed
     float speed{4.0};
+
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        winDims[0]/2.f - 4.f * (0.5f * (float)knight.width/6.0),
+        winDims[1]/2.f - 4.f * (0.5f * (float)knight.height)
+    };
 
     // Set ideal FPS and begin game loop
     SetTargetFPS(60);
@@ -38,7 +44,10 @@ int main()
         }
 
         // Draw Textures
-        DrawTextureEx(map, mapPos, 0.0, 2.0, WHITE);
+        DrawTextureEx(map, mapPos, 0.f, 2.f, WHITE);
+        Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.f * (float)knight.width/6.f, 4.f * (float)knight.height};
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
 
         // Deconstruct window
         EndDrawing();
